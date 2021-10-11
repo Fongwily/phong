f1e3a765bb15fe50eb90ec4f8bf921b24df33ceb
mrdatdepzai <phong>
2021-10-11 16:46:31 +0700

    first commit

---
+++ public/js/CONFIG.js
@@ -0,0 +1,55 @@
+const __DATA__ = {
+    'config': {
+        'title': 'INFO|Nguyễn Phong',
+        'favicon': 'home.png',
+    },
+    'cover-image': 'bia.jpg',
+    'avatar-image': 'avatar.jpg',
+    'name': 'Nguyễn Phong',
+    'skills': [
+        'Singer',
+        
+    ],
+    'bio-storys': [{
+        'image': 'admin.png',
+        'content': 'Giới thiệu',
+        'author': 'Chào Bạn 👋,tui sống tại TP Hà Nội,1 HS THPT',
+    }, ],
+    'links': [{
+            'image': 'facebook.jpg',
+            'title': 'Facebook',
+            'show-value': '@pog171206',
+            'value': 'https://www.facebook.com/pog171206/',
+            'type': 0,
+        },
+        {
+            'image': 'zalo.jpg',
+            'title': 'Zalo',
+            'show-value': 'Nguyễn Phong',
+            'value': 'https://zalo.me/0372458468',
+            'type': 0,
+        },
+
+        {
+            'image': 'gmail.jpg',
+            'title': 'Gmail',
+            'show-value': 'huuphong1712@gmail.com',
+            'value': 'huuphong1712@gmail.com',
+            'type': 1,
+        },
+        {
+            'image': 'phone.jpg',
+            'title': 'MB Bank',
+            'show-value': '0866051437',
+            'value': '0866051437',
+            'type': 1,
+        },
+        {
+            'image': 'momo.jpg',
+            'title': 'MoMo',
+            'show-value': '0866051437',
+            'value': '0866051437',
+            'type': 1,
+        }
+    ]
+};
