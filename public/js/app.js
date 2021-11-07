a7c4d385ea1a18c0c8a32ac04990dbd040bcbfac
Phong <92314851+PhonggNguyenn@users.noreply.github.com>
2021-11-07 12:17:04 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -46,11 +46,11 @@ $(document).ready(function() {
     for (let i = 0; i < skills.length; i++) {
         if (i % 2 == 0) {
             skills_html += `
-                <div class="skill-item" style="color: #EF9D64;" >${skills[i]}</div>
+                <div class="skill-item" style="color: #72cfd4;" >${skills[i]}</div>
             `;
         } else {
             skills_html += `
-                <div class="skill-item" style="color: #EF9D64;" >${skills[i]}</div>
+                <div class="skill-item" style="color: #72cfd4;" >${skills[i]}</div>
             `;
         }
     }
